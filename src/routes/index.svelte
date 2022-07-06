f74898a1064e29c386783f4d241ccae31999af71
plexcodes <ignotusplex@gmail.com>
2022-07-07 00:16:04 +0300

    Some splashscreen updates

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,22 @@
 <link rel="stylesheet" href="/fonts.css">
 <link rel="stylesheet" href="/indexstyle.css">
-<h1>welcome to psihoeasy</h1>
+<link rel="stylesheet" href="/waves">
+
+
+<div class = "content">
+<div class = "splashscreen">
+        <ul class="circles">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+        </ul>
+    <h1>welcome to psihoeasy</h1>
+</div>
+</div>
