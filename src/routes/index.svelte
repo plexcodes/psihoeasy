217696564da33e7d3572c652547cfb786bf178c0
plexcodes <ignotusplex@gmail.com>
2022-07-11 15:28:41 +0300

    Updates

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,8 @@ onMount(() => (ready = true));
 </script>
 
 <div class = "content">
-<div class = "splashscreen">
+
+<div class = "section">
         <ul class="circles">
                 <li></li>
                 <li></li>
@@ -21,9 +22,29 @@ onMount(() => (ready = true));
                 <li></li>
                 <li></li>
         </ul>
+
 {#if ready}
     <div class="img-front">
         <img  src = "/psihoeasy-splashscreen-logo.png" alt = "logo" style="width:50vw" in:fly={{ y: 500, duration: 2000 }}>
-    </div>{/if}
+    </div>
+{/if}
+
 </div>
+
+<div class="section">
+<img src = "/quote.png" alt = "quote" style = "width: 75vw; display: block; margin-left: auto; margin-right: auto; margin-top:10vh">
+<p style="margin-left:10vw;">
+    <b>Lorem ipsum</b> dolor sit amet, consectetur adipiscing elit. Donec tincidunt vulputate convallis. 
+    Etiam iaculis eros a ipsum aliquet, nec vulputate magna bibendum. Suspendisse finibus ut nisl nec luctus. 
+    Maecenas vel mi eget augue iaculis ornare vitae vel nisl. Nulla facilisi. 
+    Mauris hendrerit pharetra felis, a condimentum sem vulputate consectetur.
+</p>
+<p style="float:right; margin-right:10vw;">
+    <b>Lorem ipsum</b> dolor sit amet, consectetur adipiscing elit. Donec tincidunt vulputate convallis. 
+    Etiam iaculis eros a ipsum aliquet, nec vulputate magna bibendum. Suspendisse finibus ut nisl nec luctus. 
+    Maecenas vel mi eget augue iaculis ornare vitae vel nisl. Nulla facilisi. 
+    Mauris hendrerit pharetra felis, a condimentum sem vulputate consectetur.
+</p>
 </div>
+
+</div>  
